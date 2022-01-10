c676213ff4c9836c599d7f100fa6f204d30836b8
zcaldwell <mintyst.john@gmail.com>
2022-01-10 14:01:42 -0800

    set up files and my form, switching to dev branch cause I forgot

---
+++ src/views/Auth/Auth.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AuthForm from '../../components/AuthForm';
+import classNames from 'classnames';
+import { useState } from 'react';
+
+export default function Auth() {
+  const [type, setType] = useState('');
+  const 
+  return (
+    <div>
+      <AuthForm />
+    </div>
+  );
+}
