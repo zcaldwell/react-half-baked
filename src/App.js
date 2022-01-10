eb3c9ab1be454e071bf66a343663bb251e6959c1
zcaldwell <mintyst.john@gmail.com>
2022-01-10 14:57:22 -0800

    app.js written and debugging my auth.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import Auth from './views/Auth/Auth';
-import reactRouterDom from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { getUser } from './services/users';
+import { useState } from 'react';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(getUser());
+
   return (
-    
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            {currentUser && (
+              <>
+                <h1>Signed In</h1>
+              </>
+            )}
+            {!currentUser && <Auth setCurrentUser={setCurrentUser} />}
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/views/Auth/Auth.js
+++ src/views/Auth/Auth.js
@@ -2,13 +2,54 @@ import React from 'react';
 import AuthForm from '../../components/AuthForm';
 import classNames from 'classnames';
 import { useState } from 'react';
+import { signInUser, signUpUser } from '../../services/users';
 
 export default function Auth() {
-  const [type, setType] = useState('');
-  const 
+  const [type, setType] = useState('signin');
+  const [error, setError] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let resp;
+
+      setCurrentUser(resp);
+    } catch {
+      setError('Login Failed');
+    }
+  };
+
   return (
     <div>
-      <AuthForm />
+      <div className="header">
+        <h3
+          onClick={() => {
+            setType('signin');
+          }}
+          className={classNames({ active: type === 'signin' })}
+        >
+          Sign In
+        </h3>
+      </div>
+      <div className="header">
+        <h3
+          onClick={() => {
+            setType('signup');
+          }}
+          className={classNames({ active: type === 'signup' })}
+        >
+          Sign Up
+        </h3>
+      </div>
+      <AuthForm
+        email={email}
+        password={password}
+        setEmail={setEmail}
+        setPassword={setPassword}
+        handleSubmit={handleSubmit}
+        error={error}
+      />
     </div>
   );
 }
